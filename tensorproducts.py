8f7a6f2a30f0f035aac098084a82ecedf2da01d3
RonjaZijd <ronja.zijderveld@student.manchester.ac.uk>
2021-08-20 13:10:08 +0200

    Another message

--- tensorproducts.py
+++ tensorproducts.py
@@ -11,72 +11,47 @@ np.set_printoptions(suppress=True, precision=3, formatter={'float_kind':'{:0.2f}
 
 H_coeffs = [-0.24274280513140462,-0.24274280513140462,-0.04207897647782276,0.1777128746513994,0.17771287465139946,0.12293305056183798,0.12293305056183798,0.1676831945771896,0.1676831945771896,0.17059738328801052,0.17627640804319591,-0.04475014401535161,-0.04475014401535161,0.04475014401535161,0.04475014401535161]
 H_gates = [['Z3'],['Z4'],['I1'],['Z2'],['Z1'],['Z1', 'Z3'],['Z2','Z4'],['Z1','Z4'],['Z2','Z3'],['Z1','Z2'],['Z3', 'Z4'], ['Y1', 'Y2', 'X3', 'X4'],['X1', 'X2', 'Y3', 'Y4'], ['Y1', 'X2', 'X3', 'Y4'] ]
+tot_wires = 4
+Hamil_matrix = np.zeros(shape=(tot_wires*tot_wires, tot_wires*tot_wires), dtype = np.complex128)
+
+X = [[0, 1], [1,0]]
+I = [[1, 0], [0, 1]]
+Z = [[1, 0], [0, -1]]
+Y = [[0, -1j], [1j, 0]]
 
 
+gates_dict = {"X":X, "Y":Y, "Z":Z, "I":I}
 
-def from_gates_to_gates(wire, gate):
-    X = [[0, 1], [1,0]]
-    I = [[1, 0], [0, 1]]
-    Z = [[1, 0], [0, -1]]
-    Y = [[0, -1j], [1j, 0]]
-    G1 = I
-    G2 = I
-    G3 = I
-    G4 = I  
-    if wire==1:
-        if gate=='X':
-            G1=X
-        if gate=='Y': 
-            G1=Y
-        if gate=='Z':
-            G1=Z
-    if wire==2: 
-        if gate=='X':
-            G2=X
-        if gate=='Y':
-            G2=Y 
-        if gate=='Z':
-            G2=Z
-    if wire==3:
-        if gate=='X':
-            G3=X
-        if gate=='Y':
-            G3=Y 
-        if gate=='Z':
-            G3=Z
-    if wire==4: 
-        if gate=='X':
-            G4=X
-        if gate=='Y':
-            G4=Y 
-        if gate=='Z':
-            G4=Z
-    return G1, G2, G3, G4
-    
-def creating_sub_matrix(G1, G2, G3, G4):
-    M = np.kron(G3, G4)
-    M2 = np.kron(G2, M)
-    M3 = np.kron(G1, M2)
-    return M3 
+#matrices_on_wires = numpy.full((1, tot_wires), I)
 
-##first creating hamilmatrix of the correct size, for 4 wires this is 16 by 16
-Hamil_matrix = np.zeros(shape=(16, 16), dtype=np.complex128)
+def creating_sub_matrix(array):
+    print("Test2")
+    print(array[-2])
+    St = np.kron(gates_dict.get(str(array[-2])), gates_dict.get(str(array[-1])))
+    print("Test2.5")
+    print(St)
+    for i in range(len(array)-2):
+        St = np.kron(gates_dict.get(str(array[-3-i])), St)
+    print("Test3")
+    return St
 
 for i in range(len(H_gates)):
+    matrices_on_wires = np.full((tot_wires), "I", dtype=str)
     for j in range(len(H_gates[i])):
         as_characters = list(H_gates[i][j])
         gate = as_characters[0]
+        print("Test0")
         wire = int(as_characters[1])
-        G1, G2, G3, G4 = from_gates_to_gates(wire, gate)
-    sub_mat = creating_sub_matrix(G1,G2,G3,G4)
-    #print(sub_mat.shape)
-    #print(Hamil_matrix.shape)
-    Hamil_matrix = Hamil_matrix + H_coeffs[i]*sub_mat
+        matrices_on_wires[wire-1] = gate
+    print(matrices_on_wires)
+    print("Test1")
+    sub_mat = creating_sub_matrix(matrices_on_wires)
+    Hamil_matrix = Hamil_matrix + sub_mat
 
 eigvals, eigvecs = sp.linalg.eig(Hamil_matrix)
 print(eigvals[np.argmin(eigvals)])
 
-#print(Hamil_matrix)
+print(Hamil_matrix)
 
 #to do here: 
     #checking that it makes the hamiltonian matrix
