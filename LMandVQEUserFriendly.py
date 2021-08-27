ad2b779f0eade7f829c2ba97d4e1ac69d379a04a
RonjaZijd <86226964+RonjaZijd@users.noreply.github.com>
2021-08-27 17:16:23 +0200

    More changes

--- LMandVQEUserFriendly.py
+++ LMandVQEUserFriendly.py
@@ -15,12 +15,43 @@ np.set_printoptions(suppress=True, precision=3, formatter={'float_kind':'{:0.2f}
 U_gates = np.array([['RZ', 'RY', 'RZ'], ['RZ', 'RY', 'RZ'], ['RZ', 'RY', 'RZ'], ['RZ', 'RY', 'RZ']]) ##the U gates in order
 entangle_gates = np.array([[2,3], [2,0], [3,1]]) ###the entangled gates at the end
 shape = (4,3)
+global_minimum = 1.094 #needed to show log scale (estimate can also be filled in)
 
 
 #Hamiltonian
 H_VQE_coeffs = [-0.24274280513140462,-0.24274280513140462,0.1777128746513994,0.17771287465139946,0.12293305056183798,0.12293305056183798,0.1676831945771896,0.1676831945771896,0.17059738328801052,0.17627640804319591,-0.04475014401535161,-0.04475014401535161,0.04475014401535161,0.04475014401535161]
 H_VQE_gates = [['Z3'],['Z4'],['Z2'],['Z1'],['Z1', 'Z3'],['Z2','Z4'],['Z1','Z4'],['Z2','Z3'],['Z1','Z2'],['Z3', 'Z4'], ['Y1', 'Y2', 'X3', 'X4'],['X1', 'X2', 'Y3', 'Y4'], ['Y1', 'X2', 'X3', 'Y4'], ['X1', 'Y2', 'Y3', 'X4'] ]
 Hamilt_written_out = -0.24274280513140462*qml.PauliZ(wires=2) + -0.24274280513140462*qml.PauliZ(wires=3) +  0.1777128746513994*qml.PauliZ(wires=1)+0.17771287465139946*qml.PauliZ(wires=0)+0.12293305056183798*(qml.PauliZ(wires=0) @ qml.PauliZ(wires=2))+0.12293305056183798*(qml.PauliZ(wires=1)@qml.PauliZ(wires=3))+0.1676831945771896*(qml.PauliZ(wires=0)@qml.PauliZ(wires=3))+0.1676831945771896*(qml.PauliZ(wires=1)@qml.PauliZ(wires=2)) +0.17059738328801052*(qml.PauliZ(wires=0)@qml.PauliZ(wires=1))+0.17627640804319591*(qml.PauliZ(wires=2)@qml.PauliZ(wires=3))+-0.04475014401535161*(qml.PauliY(wires=0)@qml.PauliY(wires=1)@qml.PauliX(wires=2)@qml.PauliX(wires=3))+-0.04475014401535161*(qml.PauliX(wires=0)@qml.PauliX(wires=1)@qml.PauliY(wires=2)@qml.PauliY(wires=3))+0.04475014401535161*(qml.PauliY(wires=0)@qml.PauliX(wires=1)@qml.PauliX(wires=2)@qml.PauliY(wires=3))+0.04475014401535161*(qml.PauliX(wires=0)@qml.PauliY(wires=1)@qml.PauliY(wires=2)@qml.PauliX(wires=3))
+
+H_LiH_coeffs = [-0.096022, -0.206128,0.364746, 0.096022, -0.206128, -0.364746, -0.145438, 0.056040,  0.110811, -0.056040, 0.080334, 0.063673, 0.110811, -0.063673, -0.095216,
+-0.012585, 0.012585, 0.012585, 0.012585, -0.002667,  -0.002667, 0.002667, 0.002667, 0.007265, -0.007265, 0.007265, 0.007265,
+-0.029640, 0.002792, -0.029640, 0.002792, -0.008195, -0.001271, -0.008195, 0.028926, 0.007499, -0.001271, 0.007499, 0.009327, 
+0.029640,0.029640, 0.028926, 
+0.002792, -0.002792, -0.016781, 0.016781, -0.016781, -0.016781, -0.009327, 0.009327, -0.009327,
+-0.011962, -0.011962, 0.000247, 0.000247,
+0.039155, -0.002895, -0.009769, -0.024280, -0.008025,
+-0.039155, 0.002895, 0.024280, 
+-0.011962, 0.011962, -0.000247, 0.000247,
+0.008195, 0.001271, 
+-0.008195, 0.008195, 
+-0.001271, 0.001271, 0.008025, 
+-0.039155, -0.002895, 0.024280, -0.009769, 0.008025, 
+0.039155, 0.002895, -0.024280, 
+-0.008195, -0.001271, 
+0.008195, 0.008195,
+-0.028926, -0.007499, 
+-0.028926, -0.007499,
+-0.007499,
+0.007499, 
+0.009769, 
+-0.001271, -0.001271, 0.008025, 
+0.007499,
+-0.007499, 
+-0.009769 ]
+H_LiH_gates = [['Z1'], ['Z1', 'Z2'], ['Z2'], ['Z3'], ['Z3', 'Z4'], ['Z4'], ['Z1', 'Z3'], ['Z1', 'Z3', 'Z4'], ['Z1', 'Z4'], ['Z1', 'Z2', 'Z3'], ['Z1', 'Z2', 'Z3', 'Z4'], ['Z1', 'Z2', 'Z4'], ['Z2', 'Z3'], ['Z2', 'Z3', 'Z4'], ['Z2', 'Z4'], ['X1', 'Z2'], ['X1'], ['X3', 'Z4'], ['X3'], ['X1', 'Z2', 'X3', 'Z4'], ['X1', 'Z2', 'X3'], ['X1', 'X3', 'Z4'], ['X1', 'X3'], ['X1', 'Z2', 'Z4'], ['X1', 'Z4'], ['Z2', 'X3', 'Z4'], ['Z2', 'X3'], ['X1', 'X2'], ['X2'], ['X3', 'X4'], ['X4'], ['X1', 'X3', 'X4'], ['X1', 'X4'], ['X1', 'X2', 'X3'], ['X1', 'X2', 'X3', 'X4'], ['X1', 'X2', 'X4'], ['X2', 'X3'], ['X2', 'X3', 'X4'], ['X2', 'X4'], ['Y1', 'Y2'], ['Y3','Y4'], ['Y1', 'Y2', 'Y3', 'Y4'], ['Z1', 'X2'], ['Z3', 'X4'], ['Z1', 'Z3', 'X4'], ['Z1', 'X4'], ['Z1', 'X2', 'Z3'], ['X2', 'Z3'], ['Z1', 'X2', 'Z3', 'X4'], ['Z1', 'X2', 'X4'], ['X2', 'Z3', 'X4'], ['Z1', 'X3', 'Z4'], ['Z1', 'X3'], ['Z1', 'Z2', 'X3', 'Z4'], ['Z1', 'Z2', 'X3'], ['Z1', 'X3', 'X4'], ['Z1', 'Z2', 'X3', 'X4'], ['Z1', 'Z2', 'X4'], ['Z2', 'X3', 'X4'], ['Z2', 'X4'], ['Z1', 'Y3', 'Y4'], ['Z1', 'Z2', 'Y1', 'Y2'], ['Z2', 'Y3', 'Y4'], ['X1', 'Z2', 'Z3'], ['X1', 'Z3'], ['X1', 'Z2', 'Z3', 'Z4'], ['X1', 'Z3', 'Z4'], ['X1', 'Z2', 'X3', 'X4'], ['X1', 'Z2', 'X4'], ['X1', 'Z2', 'Y3', 'Y4'], ['X1', 'Y3', 'Y4'], ['X1', 'Z2', 'Z3', 'X4'], ['X1', 'Z3', 'X4'] , ['Z2', 'Z3', 'X4'], ['X1', 'X2', 'Z3'], ['X1', 'X2', 'Z3', 'Z4'], ['X1', 'X2', 'Z4'], ['X2', 'Z3', 'Z4'], ['X2', 'Z4'], ['Y1', 'Y2', 'Z3'], ['Y1', 'Y2', 'Z3', 'Z4'], ['Y1', 'Y2', 'Z4'], ['X1', 'X2', 'X3', 'Z4'], ['X2', 'X3', 'Z4'], ['Y1', 'Y2', 'X3', 'Z4'], ['Y1', 'Y2', 'X3'], ['X1', 'X2', 'Y3', 'Y4'], ['X2', 'Y3', 'Y4'], ['Y1', 'Y2', 'X3', 'X4'], ['Y1', 'Y2', 'X4'], ['X1', 'X2', 'Z3', 'X4'], ['Y1', 'Y2', 'Z3', 'X4'], ['Z1', 'Z2', 'Z3', 'X4'], ['Z1', 'X2', 'X3', 'Z4'], ['Z1', 'X2', 'X3'], ['Z1', 'X2', 'Z4'], ['Z1', 'X2', 'X3', 'X4'], ['Z1', 'X2', 'Y3', 'Y4'], ['Z1', 'X2', 'Z3', 'Z4'] ]   
+
+print(len(H_LiH_coeffs))
+print(len(H_LiH_gates))
 
 ### Basic Circuit in Pennylane form ###
 def circuit(params, wires):
@@ -84,7 +115,14 @@ def run_input_gui():
     n_of_its_entry.pack()
     seed_n_entry.pack()
 
+    check_2 = tk.IntVar()
+    check_3 = tk.IntVar()
+    check_button_2 = tk.Checkbutton(finished_frame, text = "Plot the energy in k-space.", variable=check_2)
+    check_button_3 = tk.Checkbutton(finished_frame, text = "Print condition number of H in k-space", variable=check_3)
     finish_but = tk.Button(finished_frame, text = "Filled everything in!", command = finished)
+    
+    check_button_2.pack()
+    check_button_3.pack()
     finish_but.pack()
 
     gui_running = True
@@ -92,9 +130,11 @@ def run_input_gui():
         inp_gui.update()
 
     try:
-        reg = int(reg_entry.get())       ### Currently no input validation!!
-        min_k = int(min_k_entry.get())
-        max_k = int(max_k_entry.get())
+        reg = float(reg_entry.get())       ### Currently no input validation!!
+        min_k = float(min_k_entry.get())
+        max_k = float(max_k_entry.get())
+        if max_k<min_k:
+            print(3/0)  ##probably a very bad way to throw an error
         n_of_k = int(n_of_k_entry.get())
         n_of_its = int(n_of_its_entry.get())
         seed_n = int(seed_n_entry.get())
@@ -108,9 +148,12 @@ def run_input_gui():
 
     inp_gui.destroy()
 
-    return reg, min_k, max_k, n_of_k, n_of_its, seed_n
+    return reg, min_k, max_k, n_of_k, n_of_its, seed_n, check_2.get(), check_3.get()
 
-reg, min_k, max_k, n_of_k, n_of_its, seed_n = run_input_gui()
+reg, min_k, max_k, n_of_k, n_of_its, seed_n, plot_k, plot_c = run_input_gui()
+
+if plot_k:
+    print("This worked!")
 
 ###########################################  Running the program ###################################################
 
@@ -253,43 +296,72 @@ for n in range(n_of_its) :
 
 ############################################ Plotting functions ########################################################
 
+energy_array_LM_log_prep = energy_array_LM + global_minimum
+energy_array_adam_log_prep = energy_array_adam + global_minimum
+energy_array_grad_log_prep = energy_array_grad + global_minimum
+energy_array_scipy_log_prep = energy_array_scip + global_minimum
+
 def plot_lm():
     fig = plt.figure()
     ploty = fig.add_subplot(111)
-    ploty.plot(n_array_LM, energy_array_LM)
-    ploty.set(xlabel='Iterations', ylabel = 'Energy')
+    ploty.set(xlabel='Iterations', ylabel = 'Energy') 
+    if check_1.get()==1:
+        ploty.plot(n_array_LM, energy_array_LM_log_prep)
+        ploty.set_yscale('log')
+    else:
+        ploty.plot(n_array_LM, energy_array_LM)
     fig.show()
 
 def plot_scipy():
     fig = plt.figure()
     ploty = fig.add_subplot(111)
-    ploty.plot(n_array_scipy, energy_array_scip)
     ploty.set(xlabel='Iterations', ylabel = 'Energy')
+    if check_1.get()==1:
+        ploty.plot(n_array_scipy, energy_array_scipy_log_prep)
+        ploty.set_yscale('log')
+    else:
+        ploty.plot(n_array_scipy, energy_array_scip)
     fig.show()
 
 def plot_adam():
     fig = plt.figure()
     ploty = fig.add_subplot(111)
-    ploty.plot(n_array_adam, energy_array_adam)
     ploty.set(xlabel='Iterations', ylabel = 'Energy')
+    if check_1.get()==1:
+        ploty.plot(n_array_adam, energy_array_adam_log_prep)
+        ploty.set_yscale('log')
+    else:
+        ploty.plot(n_array_adam, energy_array_adam)
     fig.show()
 
 def plot_grad():
     fig = plt.figure()
     ploty = fig.add_subplot(111)
-    ploty.plot(n_array_grad, energy_array_grad)
     ploty.set(xlabel='Iterations', ylabel = 'Energy')
+    if check_1.get()==1:
+        ploty.plot(n_array_grad, energy_array_grad_log_prep)
+        ploty.set_yscale('log')
+    else:
+        ploty.plot(n_array_grad, energy_array_grad)
     fig.show()
 
 def plot_all():
     fig, ax = plt.subplots(2,2)
-    ax[0,0].plot(n_array_LM, energy_array_LM)
+
+    if check_1.get()==1:
+        ax[0,0].plot(n_array_LM, energy_array_LM_log_prep)
+        ax[0,1].plot(n_array_adam, energy_array_adam_log_prep)
+        ax[1,1].plot(n_array_scipy, energy_array_scipy_log_prep)
+        ax[1,0].plot(n_array_grad, energy_array_grad_log_prep)
+    else: 
+        ax[0,0].plot(n_array_LM, energy_array_LM)
+        ax[0,1].plot(n_array_adam, energy_array_adam)
+        ax[1,0].plot(n_array_grad, energy_array_grad)
+        ax[1,1].plot(n_array_scipy, energy_array_scip)
+
     ax[0,0].set_title(f'Linear Method w/ final value: {eee}')
-    ax[0,1].plot(n_array_adam, energy_array_adam)
     ax[0,1].set_title('Adam')
-    ax[1,0].plot(n_array_grad, energy_array_grad)
     ax[1,0].set_title('Grad')
-    ax[1,1].plot(n_array_scipy, energy_array_scip)
     ax[1,1].set_title('Scipy')
 
     for axi in ax.flat:
@@ -297,6 +369,23 @@ def plot_all():
 
     plt.show()
 
+def plot_in_one_graph():
+    fig = plt.figure()
+    ploty = fig.add_subplot(111)
+    if check_1.get()==1:
+        ploty.plot(n_array_LM, energy_array_LM_log_prep, label = 'Linear Method')
+        ploty.plot(n_array_grad, energy_array_grad_log_prep, label = 'Grad')
+        ploty.plot(n_array_scipy, energy_array_scipy_log_prep, label = 'SciPy')
+        ploty.plot(n_array_adam, energy_array_adam_log_prep, label = 'Adam')
+        ploty.set_yscale('log')
+    else:
+        ploty.plot(n_array_grad, energy_array_grad, label = 'Grad')
+        ploty.plot(n_array_adam, energy_array_adam, label = 'Adam')
+        ploty.plot(n_array_scipy, energy_array_scip, label = 'SciPy')
+        ploty.plot(n_array_LM, energy_array_LM, label = 'Linear Method')
+    ploty.legend()
+    ploty.set(xlabel='Iterations', ylabel = 'Energy')
+    fig.show()
 
 ############################################# GUI for plotting results ###############################################
 
@@ -305,26 +394,41 @@ plot_gui = tk.Tk()
 plot_gui.title("GUI to plot the results")
 
 title_frame2 = tk.Frame(plot_gui)
-first_frame = tk.Frame(plot_gui)
+left_frame = tk.Frame(plot_gui)
+right_frame = tk.Frame(plot_gui)
 
 title_frame2.pack(fill = 'x')
-first_frame.pack()
+left_frame.pack(side = 'left')
+right_frame.pack(side = 'right')
+
 
 title_1 = tk.Label(title_frame2, text = "Plots", anchor = 'w', font=("Courier", 16))   
 title_1.pack()
-print("Got to this poinT!")
-button_2 = tk.Button(first_frame, text = "Show the SciPy plot", command=plot_scipy)
-button_3 = tk.Button(first_frame, text = "Show the Adam plot", command=plot_adam)
-button_4 = tk.Button(first_frame, text = "Show the Grad plot", command=plot_grad)
-button_5 = tk.Button(first_frame, text = "Show the LM plot", command=plot_lm)
-button_6 = tk.Button(first_frame, text = "Show all plots together", command = plot_all)
-button_7 = tk.Button(first_frame, text = "Done here", command = finished)
-#packing buttons into the first frame
-button_2.pack(side='left')
-button_3.pack(side = 'left')
-button_4.pack(side = 'left')
-button_5.pack(side = 'left')
+
+
+title_2 = tk.Label(left_frame, text = "Individual" )
+button_2 = tk.Button(left_frame, text = "Show the SciPy plot", command=plot_scipy)
+button_3 = tk.Button(left_frame, text = "Show the Adam plot", command=plot_adam)
+button_4 = tk.Button(left_frame, text = "Show the Grad plot", command=plot_grad)
+button_5 = tk.Button(left_frame, text = "Show the LM plot", command=plot_lm)
+
+title_2.pack()
+button_2.pack()
+button_3.pack()
+button_4.pack()
+button_5.pack()
+
+check_1 = tk.IntVar()
+check_button_1 = tk.Checkbutton(right_frame, text = "Show the y-axis on log scale.", variable=check_1)
+title_3 = tk.Label(right_frame, text = "Together")
+button_6 = tk.Button(right_frame, text = "Show all plots together", command = plot_all)
+button_8 = tk.Button(right_frame, text = "Show all optimizers in one graph", command = plot_in_one_graph)
+button_7 = tk.Button(right_frame, text = "Done here", command = finished)
+
+title_3.pack()
 button_6.pack()
+button_8.pack()
+check_button_1.pack()
 button_7.pack()
 
 gui_running = True
